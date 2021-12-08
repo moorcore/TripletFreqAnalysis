e059e2737151c7f0ea9b3fabab97c2fbd22f352e
Maxim <maks96rush@yandex.ru>
2021-12-08 18:54:20 +0300

    Add project files.

---
+++ TripletFreqAnalysis/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace TripletFreqAnalysis
+{
+
+    /*Необходимо написать консольное приложение на C#, выполняющее частотный анализ текста.
+    Входные параметры: путь к текстовому файлу.
+    Выходные результаты: вывести на экран 10 самых часто встречающихся в тексте триплетов 
+    (3 идущих подряд буквы слова) и число их повторений, и на последней строке время работы программы в миллисекундах.
+    Требования: программа должна обрабатывать текст в многопоточном режиме.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TripletCounter counter = new TripletCounter();
+
+            Console.WriteLine("Please enter the directory path below:");
+
+            string path = Console.ReadLine();
+
+            counter.ReadFileAsync(path);
+
+            var groups = counter.CountTriplets()
+                .Where(str => str.All(ch => char.IsLetter(ch)))
+                .GroupBy(str => str);
+
+            Console.WriteLine(string.Join
+                (
+                    Environment.NewLine,
+                    groups.OrderByDescending(gr => gr.Count())
+                    .Take(10).Select(gr => $"\"{gr.Key}\" occurs {gr.Count()} times")
+                ));
+        }
+    }
+}
---
+++ TripletFreqAnalysis/TripletCounter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TripletFreqAnalysis
+{
+    public class TripletCounter
+    {
+        string text = "";
+
+        public async void ReadFileAsync(string inputPath)
+        {
+            string path = inputPath;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    text = await sr.ReadToEndAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public IEnumerable<string> CountTriplets()
+        {
+            string s = text;
+            int length = 3;
+
+            for (int i = length; i <= s.Length; i++)
+                yield return s.Substring(i - length, length);
+        }
+    }
+}
